286838b74f4f50a750399e12b04e30ee87799178
denicheprasov <denicheprasov345@gmail.com>
2026-07-18 22:46:21 +0300

    Make formula images inline with text (display:inline, max-height:60px)

--- app/services/kpolyakov_parser.py
+++ app/services/kpolyakov_parser.py
@@ -41,7 +41,7 @@ class KpolyakovParser:
                     url = f"https://kpolyakov.spb.ru{src}"
                 else:
                     url = f"{IMAGE_BASE_URL}/{src}"
-                return f'<img src="{url}" class="img-fluid rounded question-image my-2" style="display:block;margin:8px auto">'
+                return f'<img src="{url}" class="img-fluid question-image" style="display:inline;vertical-align:middle;max-height:60px;margin:2px">'
             raw = re.sub(r'<img\s+src="([^"]+)"[^>]*>', resolve_img, raw)
 
             raw = re.sub(r'<a\s+href="[^"]+"[^>]*>([^<]+)</a>', r'\1', raw)
