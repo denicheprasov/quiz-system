e21d603c5c83868adcaea861e6dcea0dd90a1198
denicheprasov <denicheprasov345@gmail.com>
2026-09-24 22:44:44 +0300

    Remove unused files

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-project_path = os.path.dirname(os.path.abspath(__file__))
-if project_path not in sys.path:
-    sys.path.insert(0, project_path)
-
-from app.main import app as application
